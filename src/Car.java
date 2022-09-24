939711afe836cf332dc55572a506661faaec0f2b
Anna Molodtsova <renasafetysea@mail.ru>
2022-09-24 20:13:56 +0300

    задание 3/2

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,35 @@
-public class Car {
-    String brand;
-    String model;
-    String color;
-    String productionCountry;
-    double engineVolume;
-    int productionYear;
+import java.util.Objects;
 
-    void obgect(){
-        System.out.println(brand + model +" " + productionYear + " год выпуска.Сборка в " +productionCountry );
-        System.out.println(color + " цвет кузова. Объем двигателя " + engineVolume );
-        System.out.println();
+public class Car {
+    private String brand;
+    private String model;
+    private int productionYear;
+    private String color;
+    private String productionCountry;
+    private double engineVolume;
+
+    public Car(String brand, String model, int productionYear, String color, String productionCountry, double engineVolume) {
+        this.brand = Objects.requireNonNullElse(brand, "default");
+        this.model = Objects.requireNonNullElse(model, "default");
+        if (productionYear >= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        this.color = Objects.requireNonNullElse(color, "белый");
+        this.productionCountry = Objects.requireNonNullElse(productionCountry, "default");
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    @Override
+    public String toString () {
+        return brand +" "+ model+ " " + productionYear +
+                " год выпуска. Цвет кузова "  + color +
+                ". Произведена в " + productionCountry +
+                ". Объем двигателя " + engineVolume;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car();
-        lada.brand = "Lada";
-        lada.model = "Granta";
-        lada.productionYear = 2015;
-        lada.productionCountry = "Russia";
-        lada.color = "yellow";
-        lada.engineVolume = 1.7;
+        Car lada = new Car("Lada","Granta",2015,"yellow","Russia",1.7);
+        System.out.println(lada);
+        System.out.println();
 
-        lada.obgect();
+        Car audi = new Car("Audi","A8",2020,"black","Germany",3.0);
+        System.out.println(audi);
+        System.out.println();
 
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8";
-        audi.productionYear = 2020;
-        audi.productionCountry = "Germany";
-        audi.color = "black";
-        audi.engineVolume = 3.0;
+        Car bmw = new Car("BMW","Z8",2021,"black","Germany",0);
+        System.out.println(bmw);
+        System.out.println();
 
-        audi.obgect();
+        Car kia = new Car("Kia","Srortage", 2018, null, "South Korea",2.4);
+        System.out.println(kia);
+        System.out.println();
 
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.productionYear = 2021;
-        bmw.productionCountry = "Germany";
-        bmw.color = "black";
-        bmw.engineVolume = 3.0;
-
-        bmw.obgect();
-
-        Car kia = new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage";
-        kia.productionYear = 2018;
-        kia.productionCountry = "South Korea";
-        kia.color = "red";
-        kia.engineVolume = 2.4;
-
-        kia.obgect();
-
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.productionYear = 2016;
-        hyundai.productionCountry = "South Korea";
-        hyundai.color = "red";
-        hyundai.engineVolume = 1.6;
-
-        hyundai.obgect();
+        Car hyundai = new Car("Hyundai","Avente", 2016,"red", null, 1.6);
+        System.out.println(hyundai);
+        System.out.println();
 
 
     }
